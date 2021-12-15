f1224ed993d7bb5925611bb69bcc72f5ec4f8474
Madison Dellamea <mdellamea@lcs.on.ca>
2021-12-15 09:48:41 -0500

    Initial Commit

---
+++ AnimationExamples/AnimationExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationExamplesApp.swift
+//  AnimationExamples
+//
+//  Created by Madison Dellamea on 12/15/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationExamples/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AnimationExamples
+//
+//  Created by Madison Dellamea on 12/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
