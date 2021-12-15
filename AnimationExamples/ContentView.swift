8221e143785977d1f9d2b3dcc7caccd789b988c6
Madison Dellamea <mdellamea@lcs.on.ca>
2021-12-15 09:52:35 -0500

    I completed the first example on HackingWithSwift showing how to make an implicit animation.

--- AnimationExamples/ContentView.swift
+++ AnimationExamples/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 1.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        // implicit animation: simplest type of animation
+        Button("Tap Me") {
+            animationAmount += 1
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .scaleEffect(animationAmount)
+        .blur(radius: (animationAmount - 1) * 3)
+        .animation(.default, value: animationAmount)
     }
 }
 
